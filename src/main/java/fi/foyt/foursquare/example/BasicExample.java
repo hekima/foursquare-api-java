9dc8a501f1715211689c823a1bbb593d396b51fa
tatiana <tatiana.pontes@zahpee.com>
2014-09-24 10:30:04 -0300

    Mudanças de parâmetros.

--- src/main/java/fi/foyt/foursquare/example/BasicExample.java
+++ src/main/java/fi/foyt/foursquare/example/BasicExample.java
@@ -4,6 +4,7 @@ import fi.foyt.foursquare.api.FoursquareApi;
 import fi.foyt.foursquare.api.FoursquareApiException;
 import fi.foyt.foursquare.api.Result;
 import fi.foyt.foursquare.api.entities.CompactVenue;
+import fi.foyt.foursquare.api.entities.CompleteVenue;
 import fi.foyt.foursquare.api.entities.VenuesSearchResult;
 
 /**
@@ -25,11 +26,20 @@ public class BasicExample {
 
 	  public void searchVenues(String ll) throws FoursquareApiException {
 	    // First we need a initialize FoursquareApi. 
-	    FoursquareApi foursquareApi = new FoursquareApi("Client ID", "Client Secret", "Callback URL");
-	    
+	    FoursquareApi foursquareApi = new FoursquareApi("CGBJN4EH2J23RV3IGUCGVKC4J4DGZ5W2XJWAXRAIKJVJOFV3", "GT4LZ5Q4R4SQGSSQDFHAZZ4GSJKAZTRLHZ2QGIV0M401ZXF2", new String());
+          //foursquareKey = "CGBJN4EH2J23RV3IGUCGVKC4J4DGZ5W2XJWAXRAIKJVJOFV3";
+          //foursquareSecret = "GT4LZ5Q4R4SQGSSQDFHAZZ4GSJKAZTRLHZ2QGIV0M401ZXF2";
+          //foursquareAccessToken = "DTIM5Y0HEBLOQ1TEUSBLSVHH0U5G3JOH2DB3UB2QXFP5UF4S";
+
+
+       // String url = "https://foursquare.com/zahpee";
+        //int index = url.lastIndexOf("/");
+        String venueId = "4fc123f4e4b08acecb574b62";
+
 	    // After client has been initialized we can make queries.
-	    Result<VenuesSearchResult> result = foursquareApi.venuesSearch(ll, null, null, null, null, null, null, null, null, null, null, null, null);
-	    
+        Result<CompleteVenue> result = foursquareApi.venue(venueId);
+
+        /*
 	    if (result.getMeta().getCode() == 200) {
 	      // if query was ok we can finally we do something with the data
 	      for (CompactVenue venue : result.getResult().getVenues()) {
@@ -42,6 +52,9 @@ public class BasicExample {
 	      System.out.println("  code: " + result.getMeta().getCode());
 	      System.out.println("  type: " + result.getMeta().getErrorType());
 	      System.out.println("  detail: " + result.getMeta().getErrorDetail()); 
-	    }
+	    }*/
+
+        CompleteVenue cv = result.getResult();
+        cv.getMayor();
 	  }
 }
